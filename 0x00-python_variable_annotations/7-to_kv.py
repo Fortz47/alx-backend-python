cf9fcbcc6ab6f0ea4e20a18b15d1e0fd54dfc6a7
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-03-14 21:30:31 +0100

    Update 7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -6,7 +6,7 @@ square of the int/float v and should be annotated as a float."""
 from typing import Tuple
 
 
-def to_kv(k: str, v: int | float, ) -> Tuple[str, float]:
+def to_kv(k: str, v: int | float) -> Tuple[str, float]:
     """function to_kv that takes a string k and an int
     OR float v as arguments and returns a tuple."""
     return tuple(k, float(v * v))

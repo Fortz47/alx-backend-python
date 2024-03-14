54b522b7bd953468c471eac92a4ab9c112ecb61d
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-03-14 22:03:02 +0100

    Update 100-safe_first_element.py

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """Annotating functions"""
-from typing import Union, Sequence, Any
+from typing import Optional, Sequence, Any
 
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+    """Annotating functions"""
     if lst:
         return lst[0]
     else:

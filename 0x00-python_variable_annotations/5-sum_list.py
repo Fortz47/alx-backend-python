fcf8abfbe783d0876d57187988df1aff9f967478
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-03-14 21:26:53 +0100

    Update 5-sum_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -3,8 +3,10 @@
 input_list of floats as argument and returns their sum as a float
 """
 
+from typing import List
 
-def sum_list(input_list: list[float]) -> float:
+
+def sum_list(input_list: List[float]) -> float:
     """
     type-annotated function sum_list which takes a list
     input_list of floats as argument and returns their sum as a float
@@ -12,4 +14,4 @@ def sum_list(input_list: list[float]) -> float:
     result = 0.0
     for v in input_list:
         result += v
-    return float(result)
+    return result

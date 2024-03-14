446d8b84acd4c14504c25f38affc83ee12fa1728
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-03-14 21:23:46 +0100

    Updated 0-add.py

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,10 +2,8 @@
 """type-annotated function add that takes a float a and
 a float b as arguments and returns their sum as a float."""
 
-from typing import Float
 
-
-def add(a: Float, b: Float) -> Float:
+def add(a: float, b: float) -> float:
     """type-annotated function add that takes a float a and
     a float b as arguments and returns their sum as a float."""
     return a + b

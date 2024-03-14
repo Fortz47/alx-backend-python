b31d96e7b4a435df19ddba8dae53b8246c6008d7
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-03-14 20:59:51 +0000

    solved 10 - 11

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+"""Annotating functions"""
+from typing import Optional, Sequence, Any
+
+
+def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+    if lst:
+        return lst[0]
+    else:
+        return None
--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+"""Annotation functions"""
+
+from typing import Mapping, Any, TypeVar, Optional
+
+
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping[Any, Any], key: Any,
+                     default: Optional[T] = None):
+    """Annotation functions"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default

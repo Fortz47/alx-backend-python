891c2df033c762f0babffff3016b93c385fb4ed5
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-03-14 21:10:53 +0000

    updated task 11

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -6,7 +6,7 @@ from typing import Mapping, Any, TypeVar, Optional, Union
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping[], key: Any,
+def safely_get_value(dct: Mapping[Any, Any], key: Any,
                      default: Optional[T] = None) -> Union[Any, T]:
     """Annotation functions"""
     if key in dct:
--- 0x00-python_variable_annotations/main.py
+++ 0x00-python_variable_annotations/main.py
@@ -5,3 +5,11 @@ to_kv = __import__('7-to_kv').to_kv
 print(to_kv.__annotations__)
 print(to_kv("eggs", 3))
 print(to_kv("school", 0.02))
+#!/usr/bin/env python3
+
+safely_get_value = __import__('101-safely_get_value').safely_get_value
+annotations = safely_get_value.__annotations__
+
+print("Here's what the mappings should look like")
+for k, v in annotations.items():
+    print( ("{}: {}".format(k, v)))

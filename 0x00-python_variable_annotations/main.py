4344a77ed0b53643acdda4efb2ae5b8d4325c522
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-03-14 21:18:17 +0000

    updated task 11

--- 0x00-python_variable_annotations/main.py
+++ 0x00-python_variable_annotations/main.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python3
 
-to_kv = __import__('7-to_kv').to_kv
-
-print(to_kv.__annotations__)
-print(to_kv("eggs", 3))
-print(to_kv("school", 0.02))
-#!/usr/bin/env python3
-
 safely_get_value = __import__('101-safely_get_value').safely_get_value
 annotations = safely_get_value.__annotations__
 

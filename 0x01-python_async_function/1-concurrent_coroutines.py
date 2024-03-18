2ce2abb2e027361b93e6f46cf97eb4b5f3eb05b6
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-03-18 07:50:27 +0000

    solved task 0-4

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""async routine called wait_n that takes in 2 int arguments
+(in this order): n and max_delay. spawns wait_random n times
+with the specified max_delay wait_n should return the list of
+all the delays (float values). The list of the delays are in
+ascending order without using sort() because of concurrency"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+from typing import List
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """spawns wait_random n times 4 with the specified max_delay"""
+    tasks = [asyncio.create_task(wait_random(max_delay)) for i in range(n)]
+    result = await asyncio.gather(*tasks)
+    return result
---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""using create_task from asyncio"""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """spawns wait_random n times 4 with the specified max_delay"""
+    tasks = [asyncio.create_task(wait_random(max_delay)) for i in range(n)]
+    result = await asyncio.gather(*tasks)
+    return result

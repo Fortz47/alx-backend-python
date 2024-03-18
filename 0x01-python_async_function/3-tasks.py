b63bd6802caffe8041a315120d03012ab652fde2
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-03-18 16:05:22 +0100

    Update 3-tasks.py

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -4,10 +4,8 @@ from typing import TypeVar
 import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-T = TypeVar('_asyncio.Task')
 
-
-def task_wait_random(max_delay: int) -> T:
+def task_wait_random(max_delay: int) -> _asyncio.Task:
     """takes an integer max_delay and returns a asyncio.Task"""
     task = asyncio.create_task(wait_random(max_delay))
     return task

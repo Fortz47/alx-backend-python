0c0e8d56bd1bd4fed51b14407f36787371ef9a98
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-04-08 21:51:58 +0100

    task 3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -74,8 +74,8 @@ class TestMemoize(unittest.TestCase):
 
         with patch.object(TestClass, 'a_method') as mock_a_method:
             obj = TestClass()
-            obj.a_property()
-            obj.a_property()
+            self.assertEqual(obj.a_property(), 42)
+            self.assertEqual(obj.a_property(), 42)
 
             mock_a_method.assert_called_once()
 
